94369479319dec6e0bd8dcf23fbc1176e469668b
salhalulu123-create <salhalulu123-create@users.noreply.github.com>
2026-07-03 09:37:59 +0300

    Update WeatherHistoryActivity: Make first history item clickable to open WeatherDetails

--- app/src/main/java/com/busaradigital/cloudcast/WeatherHistoryActivity.java
+++ app/src/main/java/com/busaradigital/cloudcast/WeatherHistoryActivity.java
@@ -1,6 +1,8 @@
 package com.busaradigital.cloudcast;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,15 @@ public class WeatherHistoryActivity extends AppCompatActivity {
         
         setContentView(R.layout.activity_weather_history);
         EdgeToEdge.enable(this);
+
+        // Make the first history item clickable to navigate to WeatherDetailsActivity
+        View historyItem = findViewById(R.id.cv_history_item);
+        if (historyItem != null) {
+            historyItem.setOnClickListener(v -> {
+                Intent intent = new Intent(WeatherHistoryActivity.this, WeatherDetailsActivity.class);
+                startActivity(intent);
+            });
+        }
 
         findViewById(R.id.btn_back).setOnClickListener(v -> finish());
     }
